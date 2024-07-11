65edad2b0fc1aa800200b7ee053e0507bcc1b5bd
Jorge Leonardo Herrera Ortiz <jorge_skidrow@hotmail.com>
2024-07-10 22:35:56 -0500

    feat(configuration): Module Federation implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,4 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'mf1';
-}
+export class AppComponent {}
---
+++ src/app/components/mf-one.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mf-one',
+  standalone: true,
+  templateUrl: './mf-one.component.html',
+})
+export class MfOneComponent {
+  title = 'mf1';
+}
